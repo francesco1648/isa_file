1fc338bd62b5305c475f11b2878ed107f94f1a4a
francesco1648 <148702524+francesco1648@users.noreply.github.com>
2025-01-18 15:07:15 +0100

    stamapa la prima colonna

--- test_conv1d/test_conv1d/main.c
+++ test_conv1d/test_conv1d/main.c
@@ -5,27 +5,29 @@
 #define COLONNE_MEM 4
 #define RIGHE_MEM 128
 
+int mem[RIGHE_MEM][COLONNE_MEM] = { 0 };
+
 void conv1d_testa(); // Dichiarazione della funzione
 void replace_sample();
-void replace_filter();
+void replace_filter(int filter_block); // il blocco di filti puo essere o 0 o 1 nel caso in cui sto usando i primi 4 filtri o gli                                                                                                                       altri 4
 void print_out(uint32_t matrix[8][124], int rows, int cols);
 void print_mem_to_file(int mem[RIGHE_MEM][COLONNE_MEM], const char* filename);
 
 int main() {
-    // con1d_testa();
-    int s, fg, m, j,z, i = 0;
+    int seq_filter = 0;
+    int s=0, fg=0, m=0, j,z, i = 0;
     int inc = 0;  // Indice per accedere a F
     int bk = 0;
     int w_c = 0;
     int w_r = 0;
-    int acc[5];
+    int acc[4];
     int R[16];
-    int ACC[4];
+   
     int app;
-    int out_mat[8][4];
+    int out_mat[8][4] = { 0 };
 
     // Dichiarazione della matrice mem con le dimensioni definite
-    int mem[RIGHE_MEM][COLONNE_MEM] = { 0 };
+  
 
     // Ciclo per copiare i dati dalla matrice F a mem
     for (int bk = 0; bk < 16; bk++) {
@@ -51,40 +53,49 @@ int main() {
         }
 
     }
+    j = 0;
+    i = 0;
 
-    conv1d_testa();
-    print_mem_to_file(mem, "mem.txt");
+    conv1d_testa(); // calcolo via sw la convoluzione e la salvo in un file outpiut.txt
+   // print_mem_to_file(mem, "mem.txt");
     //---------------------------------------------------------
     // qui faccio la convoluzione
-    for(z=0;z<1;z++){
-    for (s = 0; s < 3; s++) {
+   // for(z=0;z<1;z++){
+    //for (s = 0; s < 3; s++) {
         for (fg = 0; fg < 2; fg++) {
-            for (m = 0; m < 3; m++) {
-                for (j = 0; j - 5; j++) {
-                    for (i = 0; i < 3; i++) {
+         
+            for (m = 0; m < 4; m++) {
+                for (j = 0; j < 5; j++) {
+                    for (i = 0; i < 4; i++) {
                          R[0 + 4 * i] = mem[i * 5 + j + m * 20][0] * mem[80 + i * 9 + j + s][0] ;
                          R[1 + 4 * i] = mem[i * 5 + j + m * 20][1] * mem[80 + i * 9 + j + s][1] ;
                          R[2 + 4 * i] = mem[i * 5 + j + m * 20][2] * mem[80 + i * 9 + j + s][2] ;
                          R[3 + 4 * i] =  mem[i * 5 + j + m * 20][3] * mem[80 + i * 9 + j + s][3] ;
                         acc[i] = R[0 + 4 * i] + R[1 + 4 * i] + R[2 + 4 * i] + R[3 + 4 * i];
                     }
-                    app = acc[0] + acc[1] + acc[2] + acc[3] + acc[4];
-                    out_mat[m][s] += app;
+                    app = acc[0] + acc[1] + acc[2] + acc[3];
+                    out_mat[m+fg*4][s] = out_mat[m][s] + app;
                     i = 0;
                 }
                 j = 0;
                 i = 0;
             }
-
+            if (seq_filter == 0) {
+                seq_filter = 1;
+            }
+            else {
+                seq_filter = 0;
+            }
+            replace_filter(seq_filter);
             m = 0;
 
         }
-        replace_filter();
+      //  
        
-    }
-    replace_sample();
-    print_out(out_mat, 8, 1);
-}
+   // }
+   // replace_sample();
+   // print_out(out_mat, 8, 1);
+//}
     
 
 
@@ -111,27 +122,52 @@ int main() {
 
 
 
+//faccio scambio i filtri
+void replace_filter(int filter_block) {
+    int inc = 0;
+    if (filter_block == 0) {
+        for (int bk = 0; bk < 16; bk++) {
+            for (int w_c = 0; w_c < 4; w_c++) {
+                for (int w_r = 0; w_r < 5; w_r++) {
 
-void replace_filter() {
- 
+                    mem[w_r + bk * 5][w_c] = F[inc];
+                    inc++;
+                }
+            }
+        }
+    }
+    if (filter_block == 1) {
+        for (int bk = 0; bk < 16; bk++) {
+            for (int w_c = 0; w_c < 4; w_c++) {
+                for (int w_r = 0; w_r < 5; w_r++) {
+
+                    mem[w_r + bk * 5][w_c] = F[inc+320];
+                    inc++;
+                }
+            }
+        }
+    }
 }
-
+//scambio i sample
 void replace_sample() {
 
 }
+
+
+//stampa nel file output.txt il risultato della convoluzione calcolato via sw
 void conv1d_testa() {
-    FILE* filePtr;
+    FILE* filePtr1;
     errno_t err;
 
     // Utilizza fopen_s per aprire il file in modalità scrittura
-    err = fopen_s(&filePtr, "output.txt", "w");
+    err = fopen_s(&filePtr1, "output.txt", "w");
 
     if (err != 0) {
         // Se fopen_s fallisce, restituisce un errore
         printf("Impossibile aprire il file per la scrittura.\n");
         return; // Non restituire nulla, la funzione è di tipo void
     }
-
+    int  pr_acc[16];
     int cnt = 0;
     int sample[16][128] = { 0 };
     int f0[16][5] = { 0 };
@@ -144,7 +180,7 @@ void conv1d_testa() {
     int f7[16][5] = { 0 };
     uint32_t out_matrix[8][124] = { 0 };
     
-
+    int cm1, cm2, cm3, cm4, cm5;
 
     // Riempie il campione con i dati da A
     for (int j = 0; j < 16; j++) {
@@ -209,36 +245,41 @@ void conv1d_testa() {
     
 //convoluzione
     for (int j = 0; j < 124; j++) {
-        for (int i = 0; i < 15; i++) {
-            out_matrix[0][j] += f0[i][0] * sample[i][0 + j] + f0[i][1] * sample[i][1 + j] + f0[i][2] * sample[i][2+j] + f0[i][3] * sample[i][3+j] + f0[0][4] * sample[i][4+j];
+        for (int i = 0; i < 16; i++) {
+       //     cm1 = f0[i][0] * sample[i][0 + j];
+         //   cm2 = f0[i][1] * sample[i][1 + j];
+          //  cm3 = f0[i][2] * sample[i][2 + j];
+           // cm4 = f0[i][3] * sample[i][3 + j];
+          //  cm5= f0[i][4] * sample[i][4 + j];
+            out_matrix[0][j] += f0[i][0] * sample[i][0 + j] + f0[i][1] * sample[i][1 + j] + f0[i][2] * sample[i][2 + j] + f0[i][3] * sample[i][3 + j] + f0[i][4] * sample[i][4 + j];
+        }
+
+        for (int i = 0; i < 16; i++) {
+            out_matrix[1][j] += f1[i][0] * sample[i][0 + j] + f1[i][1] * sample[i][1 + j] + f1[i][2] * sample[i][2 + j] + f1[i][3] * sample[i][3 + j] + f1[i][4] * sample[i][4 + j];
+        }
+
+        for (int i = 0; i < 16; i++) {
+            out_matrix[2][j] += f2[i][0] * sample[i][0 + j] + f2[i][1] * sample[i][1 + j] + f2[i][2] * sample[i][2 + j] + f2[i][3] * sample[i][3 + j] + f2[i][4] * sample[i][4 + j];
         }
 
         for (int i = 0; i < 15; i++) {
-            out_matrix[1][j] += f1[i][0] * sample[i][0 + j] + f1[i][1] * sample[i][1 + j] + f1[i][2] * sample[i][2 + j] + f1[i][3] * sample[i][3 + j] + f1[0][4] * sample[i][4 + j];
+            out_matrix[3][j] += f3[i][0] * sample[i][0 + j] + f3[i][1] * sample[i][1 + j] + f3[i][2] * sample[i][2 + j] + f3[i][3] * sample[i][3 + j] + f3[i][4] * sample[i][4 + j];
         }
 
-        for (int i = 0; i < 15; i++) {
-            out_matrix[2][j] += f2[i][0] * sample[i][0 + j] + f2[i][1] * sample[i][1 + j] + f2[i][2] * sample[i][2 + j] + f2[i][3] * sample[i][3 + j] + f2[0][4] * sample[i][4 + j];
+        for (int i = 0; i < 16; i++) {
+            out_matrix[4][j] += f4[i][0] * sample[i][0 + j] + f4[i][1] * sample[i][1 + j] + f4[i][2] * sample[i][2 + j] + f4[i][3] * sample[i][3 + j] + f4[i][4] * sample[i][4 + j];
         }
 
-        for (int i = 0; i < 15; i++) {
-            out_matrix[3][j] += f3[i][0] * sample[i][0 + j] + f3[i][1] * sample[i][1 + j] + f3[i][2] * sample[i][2 + j] + f3[i][3] * sample[i][3 + j] + f3[0][4] * sample[i][4 + j];
+        for (int i = 0; i < 16; i++) {
+            out_matrix[5][j] += f5[i][0] * sample[i][0 + j] + f5[i][1] * sample[i][1 + j] + f5[i][2] * sample[i][2 + j] + f5[i][3] * sample[i][3 + j] + f5[i][4] * sample[i][4 + j];
         }
 
-        for (int i = 0; i < 15; i++) {
-            out_matrix[4][j] += f4[i][0] * sample[i][0 + j] + f4[i][1] * sample[i][1 + j] + f4[i][2] * sample[i][2 + j] + f4[i][3] * sample[i][3 + j] + f4[0][4] * sample[i][4 + j];
+        for (int i = 0; i < 16; i++) {
+            out_matrix[6][j] += f6[i][0] * sample[i][0 + j] + f6[i][1] * sample[i][1 + j] + f6[i][2] * sample[i][2 + j] + f6[i][3] * sample[i][3 + j] + f6[i][4] * sample[i][4 + j];
         }
 
-        for (int i = 0; i < 15; i++) {
-            out_matrix[5][j] += f5[i][0] * sample[i][0 + j] + f5[i][1] * sample[i][1 + j] + f5[i][2] * sample[i][2 + j] + f5[i][3] * sample[i][3 + j] + f5[0][4] * sample[i][4 + j];
-        }
-
-        for (int i = 0; i < 15; i++) {
-            out_matrix[6][j] += f6[i][0] * sample[i][0 + j] + f6[i][1] * sample[i][1 + j] + f6[i][2] * sample[i][2 + j] + f6[i][3] * sample[i][3 + j] + f6[0][4] * sample[i][4 + j];
-        }
-
-        for (int i = 0; i < 15; i++) {
-            out_matrix[7][j] += f7[i][0] * sample[i][0 + j] + f7[i][1] * sample[i][1 + j] + f7[i][2] * sample[i][2 + j] + f7[i][3] * sample[i][3 + j] + f7[0][4] * sample[i][4 + j];
+        for (int i = 0; i < 16; i++) {
+            out_matrix[7][j] += f7[i][0] * sample[i][0 + j] + f7[i][1] * sample[i][1 + j] + f7[i][2] * sample[i][2 + j] + f7[i][3] * sample[i][3 + j] + f7[i][4] * sample[i][4 + j];
         }
 
 
@@ -253,18 +294,18 @@ void conv1d_testa() {
   
     for (int i = 0; i < 8; i++) {  // Righe di out_matrix
         for (int j = 0; j < 128; j++) {  // Colonne di out_matrix
-            fprintf(filePtr, "%d ", out_matrix[i][j]);
+            fprintf(filePtr1, "%d ", out_matrix[i][j]);
         }
         // Non c'è bisogno di fprintf(filePtr, "\n"); per scrivere una nuova riga nel formato binario
     }
 
     // Chiude il file
-    fclose(filePtr);
-    return;
+    fclose(filePtr1);
+
 
 }
 
-// Funzione per stampare una matrice di tipo uint32_t
+// stampo a schermo la matrice
 void print_out(uint32_t matrix[8][124], int rows, int cols) {
     // Ciclo sulle righe
     for (int i = 0; i < rows; i++) {
@@ -277,7 +318,8 @@ void print_out(uint32_t matrix[8][124], int rows, int cols) {
 }
 
 
-
+//scrivo la matrice in un file.txt; mi serve per, attraverso matlab, scrivo l'excel che mi mostra come è organizzata attualmente 
+//la memoria del mio conv1d
 void print_mem_to_file(int mem[RIGHE_MEM][COLONNE_MEM], const char* filename) {
     FILE* filePtr;
     errno_t err;
